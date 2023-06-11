48ac8b21c322b0cec3e460c9a1624e3b0736c213
ardaarslan47 <a.ardaarslan@hotmail.com>
2023-06-11 21:37:24 +0300

    full crud task, full crud row added

--- task-assignment-app/app.js
+++ task-assignment-app/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
-const Task = require("./models/tasks");
+const { Task, Row, ContentOfRow } = require("./models/tasks");
 
 main().catch((err) => console.log(err));
 
@@ -21,17 +21,20 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
-app.get("/tasks", async (req, res) => {
+// show index
+app.get("/", async (req, res) => {
   const tasks = await Task.find({});
   res.render("index", { tasks });
 });
 
+// create task
 app.post("/tasks", async (req, res) => {
   const newTask = new Task(req.body);
   await newTask.save();
   res.redirect(`/tasks/${newTask._id}`);
 });
 
+// show single task
 app.get("/tasks/:id", async (req, res) => {
   const { id } = req.params;
   const tasks = await Task.find({});
@@ -39,16 +42,58 @@ app.get("/tasks/:id", async (req, res) => {
   res.render("show", { task, tasks });
 });
 
-app.put('/tasks/:id', async (req, res) => {
-  const {id} = req.params
-  await Task.findByIdAndUpdate(id, {$set: {title: req.body.title}})
-  res.redirect(`/tasks/${id}`)
-})
+// update task title
+app.put("/tasks/:id", async (req, res) => {
+  const { id } = req.params;
+  await Task.findByIdAndUpdate(id, { $set: { title: req.body.title } });
+  res.redirect(`/tasks/${id}`);
+});
 
+// delete task
 app.delete("/tasks/:id", async (req, res) => {
   const { id } = req.params;
   await Task.findByIdAndDelete(id);
-  res.redirect("/tasks");
+  res.redirect("/");
+});
+
+// create new row
+app.patch("/tasks/:id", async (req, res) => {
+  const { id } = req.params;
+  const { rowTitle } = req.body;
+  await Task.findByIdAndUpdate(id, {
+    $push: { content: new Row({ title: rowTitle }) },
+  });
+  res.redirect(`/tasks/${id}`);
+});
+
+// update row title
+app.put("/tasks/:id/:rowId", async (req, res) => {
+  const { id, rowId } = req.params;
+  const { newTitle } = req.body;
+  await Task.updateOne(
+    { _id: id, "content._id": rowId },
+    { $set: { "content.$.title": newTitle } }
+  );
+  res.redirect(`/tasks/${id}`);
+});
+
+// delete row
+app.delete("/tasks/:id/:rowId", async (req, res) => {
+  const { id, rowId } = req.params;
+  await Task.findByIdAndUpdate(id, { $pull: { content: { _id: rowId } } });
+  res.redirect(`/tasks/${id}`);
+});
+
+// create content of row
+app.patch("/tasks/:id/:rowId", async (req, res) => {
+  const { id, rowId } = req.params;
+  const { contentTitle, contentDescription } = req.body;
+  const newContent = new ContentOfRow({title: contentTitle, description: contentDescription})
+  await Task.updateOne(
+    { _id: id, "content._id": rowId },
+    {$push: {"content.$.content": newContent}}
+  )
+  res.redirect(`/tasks/${id}`);
 });
 
 app.listen(port, () => {
--- task-assignment-app/models/tasks.js
+++ task-assignment-app/models/tasks.js
@@ -1,11 +1,33 @@
 const mongoose = require("mongoose");
 
-const taskSchema = new mongoose.Schema({
+const contentOfRowSchema = new mongoose.Schema({
   title: {
+    type: String,
+    required: true,
+  },
+  description: {
     type: String,
     required: true,
   },
 });
 
+const contentSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: [contentOfRowSchema],
+});
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: [contentSchema],
+});
+
+const ContentOfRow = mongoose.model("ContentOfRow", contentOfRowSchema);
+const Row = mongoose.model("Row", contentSchema);
 const Task = mongoose.model("Task", taskSchema);
-module.exports = Task;
+module.exports = { Task, Row, ContentOfRow };
